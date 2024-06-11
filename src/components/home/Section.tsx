49bf658a24a8684694878f72adfce06bda9dcbef
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2024-06-11 16:53:27 +0300

    refactor sections

---
+++ src/components/home/Section.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type SectionProps = {
+  children: React.ReactNode;
+};
+
+export const Section = ({ children }: SectionProps) => {
+  return (
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 min-h-[calc(100vh-126x)]">
+      <div className="container px-4 md:px-6">{children}</div>
+    </section>
+  );
+};
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 import Logo from "../../assets/logo.svg?react";
 export const Header = () => {
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center border-b border-gray-50">
+    <header className="px-4 lg:px-6 h-14 flex items-center">
       <Link href="#" className="flex items-center justify-center">
         <Logo className="h-36 w-auto" />
       </Link>
