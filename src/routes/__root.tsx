b2af168ee3603a305559934fecb800ebc0ae09f2
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2024-06-11 17:13:08 +0300

    added vercel

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,16 +2,22 @@ import { Header } from "@/components/layout/Header";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster } from "@/components/ui/toaster";
+import { mountVercelToolbar } from "@vercel/toolbar";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Footer } from "@/components/layout/Footer";
+import { useEffect } from "react";
 
 export const Route = createRootRoute({
   component: Root,
 });
 
 function Root() {
+  useEffect(() => {
+    mountVercelToolbar();
+  }, []);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
