3d755ec982f7fd0cd068568c1a5e6d8f40b1d260
lukevenediger <lukevenediger@users.noreply.github.com>
2024-06-12 22:24:39 +0200

    working on svg centering

--- src/components/home/Section2.tsx
+++ src/components/home/Section2.tsx
@@ -6,19 +6,16 @@ import ThePowerOfDAOCommunity from "../../assets/power_of_dao.svg?react"
 const sectionsData = [
   {
     title: "Start CloudDAO business",
-    imageIndex: 0,
     description:
       "Create your permissionless open cloud businesses in minutes. Offer full transparency and alignment of interests by simply incorporating in a decentralized way.",
   },
   {
     title: "Abstracted Cloud Operations",
-    imageIndex: 1,
     description:
       "Codify and automate every aspect of your cloud business, utilizing the era of cloud-native, open-source economy and decentralized infrastructure.",
   },
   {
     title: "The Power Of DAO Community",
-    imageIndex: 2,
     description:
       "Promote project funding, product development, and business sustainability through a genuinely democratic, collaborative, and incentive-based approach.",
   },
@@ -30,14 +27,14 @@ export const Section2 = () => {
       <div className="flex flex-col items-center justify-center space-y-4 text-center">
         <div className="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-3 lg:gap-12">
           {sectionsData.map((section, index) => (
-            <div className="grid gap-1" key={index}>
-              {section.imageIndex === 0 && <StartCloudDAO 
+            <div className="grid gap-1 text-center justify-items-center" key={index}>
+              {index === 0 && <StartCloudDAO 
                 className="w-48 h-48 fill-current text-white"
                 fill="currrentColor"/>}
-              {section.imageIndex === 1 && <AbstractedCloudOperations
+              {index === 1 && <AbstractedCloudOperations
                 className="w-48 h-48 fill-current text-white"
                 fill="currentColor"/>}
-              {section.imageIndex === 2 && <ThePowerOfDAOCommunity
+              {index === 2 && <ThePowerOfDAOCommunity
                 className="w-48 h-48 fill-current text-white"
                 fill="currentColor"/>}
 
