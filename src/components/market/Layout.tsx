ec159b55049fef468128a73d566db8aa24e0e29d
lukevenediger <lukevenediger@users.noreply.github.com>
2024-06-12 22:56:47 +0200

    fixing padding

--- src/components/market/Layout.tsx
+++ src/components/market/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex gap-4 min-h-[calc(100dvh-56px)] p-4">
       <SideBar />
-      <Card className="flex-1 bg-gray-100 dark:bg-gray-800 w-full py-12 md:py-24 lg:py-32 flex flex-col gap-24 p-4">
+      <Card className="flex-1 bg-gray-100 dark:bg-gray-800 w-full py-12 flex flex-col gap-24 p-4">
         {children}
       </Card>
     </div>
