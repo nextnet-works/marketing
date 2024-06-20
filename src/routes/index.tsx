3600b5effaff1e1475163343d24a0f13e31ecbe6
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2024-06-20 20:03:24 +0300

    a

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Layout } from "@/components/home/Layout";
-import { Section1 } from "@/components/home/Section1";
-import { Section2 } from "@/components/home/Section2";
-import { Section3 } from "@/components/home/Section3";
-import { Section4 } from "@/components/home/Section4";
-import { Section5 } from "@/components/home/Section5";
-import Section6 from "@/components/home/Section6";
+// import { Section1 } from "@/components/home/Section1";
+// import { Section2 } from "@/components/home/Section2";
+// import { Section3 } from "@/components/home/Section3";
+// import { Section4 } from "@/components/home/Section4";
+// import { Section5 } from "@/components/home/Section5";
+// import Section6 from "@/components/home/Section6";
 import { useHeaderController } from "@/components/layout/HeaderControllerContext";
+import { TypographyH1 } from "@/components/typography/h1";
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect } from "react";
 
@@ -22,12 +23,7 @@ function Home() {
 
   return (
     <Layout>
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
+      <TypographyH1>Coming soon</TypographyH1>
     </Layout>
   );
 }
