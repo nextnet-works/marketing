0c95ff474bc6e3e355e7a4565ec9275708e373d5
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2024-06-23 19:31:56 +0300

    fix loader to have same color

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,14 @@
 import { LoaderIcon } from "lucide-react";
 
 export const Loader = () => (
-  <div
-    className="m-auto flex flex-col items-center justify-center gap-8
-  position absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+  <div className="w-[100dvw] h-[100dvh] bg-[#020917] position fixed top-0 flex">
+    <div
+      className="m-auto flex flex-col items-center justify-center gap-8 
+  position absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white
   "
-  >
-    <LoaderIcon size={64} className="animate-spin m-auto" />
-    <h1 className="text-4xl">Loading...</h1>
+    >
+      <LoaderIcon size={64} className="animate-spin m-auto" />
+      <h1 className="text-4xl">Loading...</h1>
+    </div>
   </div>
 );
